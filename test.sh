24803dda0dff2a083d060d44c2693aa3d8376c72
Maxim Devoir <maximdevoir@gmail.com>
2019-11-02 11:42:56 -0700

    More logs

--- test.sh
+++ test.sh
@@ -33,12 +33,14 @@ _yarn_scripts() {
   commands=($scripts $binaries)
 
 
+  echo "binaries first element: $binaries"
   echo -e "\n\nbinaries are"
   for bin in "${binaries[@]}"
   do
     echo "$bin"
   done
 
+  echo "script names first element: $scriptNames"
   echo -e "\n\nscript names are:"
   for sn in "${scriptNames[@]}"
   do
